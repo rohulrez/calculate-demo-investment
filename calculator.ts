3dce23fae14146c41c623bc55b3074bd76c13d83
rohulrez <rezayeerohullah@gmail.com>
2025-05-20 08:58:12 -0700

    Application logic added

--- calculator.ts
+++ calculator.ts
@@ -8,14 +8,45 @@ duration: number
 type InvestmentResult = {
     year: string,
     totalAmount: number,
-    totalContribution: number,
+    totalContributions: number,
     totalIntrestEarned: number
 }
 
 type CalculationResult = InvestmentResult[] | string;
 
 function calculateInvestment (data: InvestmentData): CalculationResult{
+    const {initialAmount, annualContrib, expectedReturn, duration} = data;
 
+    if (initialAmount < 0) {
+        return 'Initial investment amount must  not be zero.'
+    }
+    if (duration <= 0) {
+        return 'No valid amount of years provided!'
+    }
+    if (expectedReturn < 0) {
+        return 'Expected return must be at least zero.'
+    }
+
+    let total = initialAmount;
+    let totalContributions = 0;
+    let totalIntrestEarned = 0;
+
+    const annualResult: InvestmentResult[] =[];
+
+    for (let i =0; i< duration; i++) {
+        total = total * (1 + expectedReturn);
+        totalIntrestEarned = total - totalContributions - initialAmount;
+        totalContributions = totalContributions + annualContrib;
+        total = total + annualContrib;
+
+        annualResult.push({
+            year : `Year ${i + 1}`,
+            totalAmount: total,
+            totalIntrestEarned,
+            totalContributions,  
+        })
+    }
+    return annualResult;
 }
 
 
